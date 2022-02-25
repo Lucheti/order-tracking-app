62c1b3abdc1f4ca9632678223a6af62ae3042497
lucheti <lucas.garcia@mulesoft.com>
2022-02-24 22:29:41 -0300

    suspense react

--- blitz.config.ts
+++ blitz.config.ts
@@ -7,9 +7,6 @@ const config: BlitzConfig = {
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
-  experimental: {
-    reactRoot: false,
-  },
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
